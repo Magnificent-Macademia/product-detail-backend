fa3368a14e1cc799f7c621e7cd3bb6ed79d97875
Darya Kutovaya <kutovayadarya@gmail.com>
2022-01-13 19:44:46 -0800

    rough draft of query logic

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,31 @@ const { Pool, Client } = require('pg');
 const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
 const pool = new Pool({
   connectionString,
-})
+});
+
+
+//getAllProducts
+//getOneProduct
+//getStyles
+//getRelated
+//getPhoto
+//getFeatures
+//getSkus
+
+const getAllProducts  = (callback) => {
+  var selectString = 'SELECT * FROM products';
+  pool.query(selectString, (err, results) => {
+    if (err) {
+      console.log(`${err} : unable to retrieve products from the database`);
+    } else {
+      // console.log('Results from the database:');
+      // console.log(results);
+      callback(null, results);
+      //pool.end()
+    }
+  });
+}
+
 pool.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   pool.end()
@@ -42,3 +66,5 @@ client.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res)
 //   client.end()
 // })
+
+module.exports = {getAllProducts};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const path = require('path');
-
+//import db from index.js
 const express = require('express');
 
 const app = express();
-
-//const controllers = require('./controllers/controllers');
 
 const PORT = 3000;
 
@@ -13,18 +11,55 @@ const PORT = 3000;
 // Middleware
 // app.use(express.static(staticPath));
 app.use(express.json());
-app.use(compression());
 
 // PRODUCTS API
 
+//query to get all products
+//query to get 1 product, required query param product_id: ex.63609
+//query to get product styles, required query param product_id: ex.63609
+//query to get related products, required query param product_id: ex.63609
+//query to get product photo, required query param style_id: ex.12345
+// query to get product features, required query param product_id: ex.63609
+// query to get product style skus, required query param style_id: ex.12345
+
+// EXAMPLE:
+//to get pictute of item ID 63609 with style 12345:
+//call get styles with where product id is 63609, select all from styles where style_id is the query,
+//query photo table with that style_id to get thumbnail or regular photo
+
 // GET ALL PRODUCTS
-app.get('/products', );
+app.get('/products', (req, res) => {
+  db.getAllProducts((err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      // console.log('Server is receiving data:');
+      // console.log(data);
+      res.send(data);
+    }
+  });
+});
 
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
+//GET ONE PRODUCT
+app.get('/products/get', (req, res) => {
+  var idObj = req.query;
+  db.getOneProduct(idObj, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      // console.log('Server is receiving data for one product:');
+      // console.log(data);
+      res.send(data);
+    }
+  });
+});
 
 
+// app.get('/', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
 
 
 app.listen(port, () => {
